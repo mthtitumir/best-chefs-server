bad8018999d51f932917491af8441a614a3a71f1
mthtitumir <titumirgccc@gmail.com>
2023-05-02 03:15:14 +0600

    chefs data loaded

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 7000;
+const chefs = require('./data/chefs.json');
 
 
 app.get('/', (req, res) => {
   res.send('Chefs are cooking')
+})
+app.get('/chefs', (req, res)=>{
+    res.send(chefs);
 })
 
 
