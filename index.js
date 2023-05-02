771c1d1a4ba46379800b6a226a5bfbf7ed692376
mthtitumir <titumirgccc@gmail.com>
2023-05-03 00:49:30 +0600

    chefs data updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 7000;
 const chefs = require('./data/chefs.json');
+const foods = require('./data/foods.json');
 const recipes = require('./data/recipes.json');
 const categories = require('./data/foodCategory.json');
 const cors = require('cors')
@@ -12,8 +13,16 @@ app.get('/', (req, res) => {
 app.get('/chefs', (req, res)=>{
     res.send(chefs);
 })
+app.get('/foods', (req, res)=>{
+    res.send(foods);
+})
 app.get('/recipes', (req, res)=>{
     res.send(recipes);
+})
+app.get('/recipes/:id', (req, res)=>{
+  const id = req.params.id;
+  const chefsRecipes = recipes.filter(recipe => recipe.category == id);
+  res.send(chefsRecipes);
 })
 app.get('/categories', (req, res)=>{
     res.send(categories);
