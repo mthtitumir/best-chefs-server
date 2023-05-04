0a43f3a661691d1dee42dbc6f6e55a7674b26a16
mthtitumir <titumirgccc@gmail.com>
2023-05-04 16:35:36 +0600

    last commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const chefs = require('./data/chefs.json');
 const foods = require('./data/foods.json');
 const recipes = require('./data/recipes.json');
 const categories = require('./data/foodCategory.json');
+const blogs = require('./data/blogs.json');
 const cors = require('cors')
 app.use(cors())
 app.get('/', (req, res) => {
@@ -18,6 +19,9 @@ app.get('/foods', (req, res)=>{
 })
 app.get('/recipes', (req, res)=>{
     res.send(recipes);
+})
+app.get('/blogs', (req, res)=>{
+    res.send(blogs);
 })
 app.get('/recipes/:id', (req, res)=>{
   const id = req.params.id;
